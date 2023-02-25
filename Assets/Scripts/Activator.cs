b30e0522ca34d96ec2e5ab9b9de79f60b5f53fea
CaptPenguin <tzelde1@lsu.edu>
2023-02-25 15:04:56 -0600

    First Commit

---
+++ Assets/Scripts/Activator.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Activator : MonoBehaviour
+{
+    public KeyCode key;
+    bool active = false;
+    GameObject note;
+    SpriteRenderer sr;
+    Color old;
+    public bool createMode;
+    public GameObject n;
+
+
+    void Awake(){
+        sr=GetComponent<SpriteRenderer>();
+    }
+
+    void Start(){
+        old=sr.color;
+    }
+
+    void Update()
+    {
+        if(createMode){
+            if(Input.GetKeyDown(key)){
+                Instantiate(n,transform.position,Quaternion.identity);
+            }
+        }else{
+
+        if(Input.GetKeyDown(key)){
+            StartCoroutine(Pressed());
+        }
+        if(Input.GetKeyDown(key)&&active){
+            Destroy(note);
+            AddScore();
+            active=false;
+        }
+    
+    }
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        active = true;
+        if(col.gameObject.tag=="Note")
+            note=col.gameObject;
+    }
+
+    void OnTriggerExit2D(Collider2D col){
+    active = false;
+    }
+
+    void AddScore(){
+        PlayerPrefs.SetInt("Score",PlayerPrefs.GetInt("Score")+100);
+    }
+
+    IEnumerator Pressed(){
+        sr.color = new Color(0,0,0);
+        yield return new WaitForSeconds(0.05f);
+        sr.color = old;
+    }
+}
