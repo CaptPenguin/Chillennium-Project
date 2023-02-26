6f7426c8de6d2873f9bf978e1d73ac70cbd7d413
CaptPenguin <tzelde1@lsu.edu>
2023-02-25 19:54:26 -0600

    Added Multiplier

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,6 +21,33 @@ public class GameManager : MonoBehaviour
         
     }
 
+    public void AddStreak(){
+        streak++;
+        if(streak >= 24){
+            multiplier=4;
+        }
+        else if(streak >= 16){
+            multiplier=3;
+        }
+        else if(streak >= 8){
+            multiplier=2;
+        }
+        else{
+            multiplier = 1;
+        }
+    }
+
+    public void ResetStreak(){
+        streak=0;
+        multiplier=1;
+    }
+
+    void UpdateGUI(){
+        PlayerPrefs.SetInt("Streak",streak);
+        PlayerPrefs.SetInt("Multi",multiplier);
+        UpdateGUI();
+    }
+
     public int GetScore(){
         return 100*multiplier;
     }
